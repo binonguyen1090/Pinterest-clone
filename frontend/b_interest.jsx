ef71a53193e2b42a3442d06bccdcb0747621add8
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-02 14:27:29 -0800

    react display con localhost

---
+++ frontend/b_interest.jsx
@@ -0,0 +1,9 @@
+// frontend/bench_bnb.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to React_redux</h1>, root);
+});
