790346183104b30671feb7f070c590132c49c146
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-02 17:46:47 -0800

    Add actions, reducers and store fro FrontEnd

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,24 @@
+//postUser , postSession, destrou
+
+export const postUser = (user) => {
+    return $.ajax({
+        url: "api/users",
+        method: "POST",
+        data: { user }
+    })
+}
+
+export const postSession = (user) => {
+    return $.ajax({
+        url: "api/session",
+        method: "POST",
+        data: { user }
+    })
+}
+
+export const destroySession = () => {
+    return $.ajax({
+        url: "api/session",
+        method: "DELETE"
+    })
+}
