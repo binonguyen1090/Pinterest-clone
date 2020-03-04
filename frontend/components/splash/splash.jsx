980c0ae5426e5f7d09b9a731228088ca19e293e1
Bino Nguyen <binonguyen1090@gmail.com>
2020-03-04 02:29:33 -0800

    add favicon and image for splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,42 +6,43 @@ export default class Splash extends React.Component {
     render(){
         return (
           <div className="all-splash">
-            <div className="splash title">
+            <div className="splash-title">
               <h1>Get ur next idea</h1>
             </div>
-            
-            <div className="containerSplashImg">
-              <div className="example first">
-                <img className="imgSplash" src="assets/dress2" />
-                <img className="imgSplash" src="assets/dress5" />
-              </div>
-              <div className="example">
-                <img className="imgSplash" src="assets/dress3" />
-                <img className="imgSplash" src="assets/dress4" />
-              </div>
-              <div className="example">
-                <img className="imgSplash" src="assets/dress2" />
-                <img className="imgSplash" src="assets/dress5" />
-              </div>
-              <div className="example">
-                <h2 className="exempletext">Yo</h2>
-              
-                <img className="imgSplash" src="assets/dress3" />
-                <img className="imgSplash" src="assets/dress4" />
-              </div>
-              <div className="example">
-                <img className="imgSplash" src="assets/dress2" />
-                <img className="imgSplash" src="assets/dress5" />
-              </div>
-              <div className="example">
-                <img className="imgSplash" src="assets/dress3" />
-                <img className="imgSplash" src="assets/dress4" />
-              </div>
-              <div className="example last">
-                <img className="imgSplash" src="assets/dress2" />
-                <img className="imgSplash" src="assets/dress5" />
-              </div>
 
+            <div className="containerSplashImg">
+              <div className="divSplashpics">
+                <img className="imgSplash" src="assets/splash11" />
+                <img className="imgSplash" src="assets/splash6" />
+              </div>
+              <div className="divSplashpics">
+                <div className="dummy1"></div>
+                <img className="imgSplash" src="assets/dress2" />
+                <img className="imgSplash t" src="assets/splash3" />
+              </div>
+              <div className="divSplashpics">
+                <div className="dummy2"></div>
+                <img className="imgSplash" src="assets/splash7" />
+                <img className="imgSplash" src="assets/splash5" />
+              </div>
+              <div className="divSplashpics">
+                <h2 className="textinsplash">Yo</h2>
+                <img className="imgSplash" src="assets/dress3" />
+              </div>
+              <div className="divSplashpics">
+                <div className="dummy2"></div>
+                <img className="imgSplash" src="assets/splash10" />
+                <img className="imgSplash" src="assets/splash4" />
+              </div>
+              <div className="divSplashpics">
+                <div className="dummy1"></div>
+                <img className="imgSplash" src="assets/splash8" />
+                <img className="imgSplash t" src="assets/dress4" />
+              </div>
+              <div className="divSplashpics last">
+                <img className="imgSplash" src="assets/splash9" />
+                <img className="imgSplash" src="assets/dress5" />
+              </div>
             </div>
 
             <div className="splashFooter">
@@ -71,3 +72,7 @@ export default class Splash extends React.Component {
         );
     }
 }
+
+
+
+
